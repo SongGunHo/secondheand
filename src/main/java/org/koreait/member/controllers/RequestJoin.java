0e7f47fa4fa35da06c9ec070ca4f12cde0131c7d
SongGeunHo <104110883+SongGunHo@users.noreply.github.com>
2025-06-06 19:17:09 +0900

    application 파일 설정 완료

---
+++ src/main/java/org/koreait/member/controllers/RequestJoin.java
@@ -0,0 +1,20 @@
+package org.koreait.member.controllers;
+
+import lombok.Data;
+
+@Data
+public class RequestJoin {
+
+    private String email;
+    private String password;
+    private String mobile;
+    private String name;
+
+
+
+
+
+
+
+
+}
